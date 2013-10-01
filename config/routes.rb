b5d241aa7802abd5d96ab0aeb89be423d1c0e97b
Paul Barry <mail@paulbarry.com>
2013-10-01 15:32:40 -0400

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Betastore::Application.routes.draw do
-  resources :subscriptions
+  resources :products, only: [:index, :show]
+  resources :subscriptions, only: [:new, :create, :show]
+  resources :orders do
+    resource :refund
+  end
   root :to => 'subscriptions#new'
 end
