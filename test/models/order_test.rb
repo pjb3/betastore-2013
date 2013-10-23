ebede08dfd74d114a39c26c9577038b547e34183
Paul Barry <mail@paulbarry.com>
2013-10-23 07:57:20 -0400

    A more realistic set of unit tests

--- test/models/order_test.rb
+++ test/models/order_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class OrderTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "an order place yesterday is returned by recent" do
+    assert Order.recent.where(id: orders(:yesterday).id).exists?
+  end
+
+  test "an order placed 10 days ago is not returned by recent" do
+    assert !Order.recent.where?(id: orders(:older).id).exists?
+  end
 end
