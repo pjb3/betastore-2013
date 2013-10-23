9c63aaf6da185d1b44c83f75eb4dde31f663526a
Paul Barry <mail@paulbarry.com>
2013-10-23 07:53:38 -0400

    Fix whitespace

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,10 +1,10 @@
 class Product < ActiveRecord::Base
   has_many :line_items
   has_many :orders, :through => :line_items
-  
+
   validates :name, presence: true
   validates :price, numericality: { greater_than_or_equal_to: 0 }
-  
+
   def image_url
     "/products/#{name.downcase.tr(' ', '_')}.jpg"
   end
