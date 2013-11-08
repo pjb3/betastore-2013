6cf78ed5518fbdc1d785f2037cfb20fef2cbf9f3
Paul Barry <mail@paulbarry.com>
2013-11-07 19:16:31 -0500

    Added data needed to print order details

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,15 @@
 class OrdersController < ApplicationController
   skip_before_filter :require_log_in, only: [:new, :create]
 
+  def new
+    @order = Order.new
+    Hash(session[:cart]).each do |pid, qty|
+      @order.line_items.build(
+        product_id: pid,
+        quantity: qty)
+    end
+  end
+
   def create
     redirect_to root_path, notice: 'Your order has been placed'
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,16 @@
 class Order < ActiveRecord::Base
   belongs_to :customer
   has_many :line_items
-  
+
   def self.recent
     where("placed_at > ?", 7.days.ago)
   end
-  
+
   def self.by(customer)
     where(customer_id: customer.id)
+  end
+
+  def total_price
+    0.0
   end
 end
