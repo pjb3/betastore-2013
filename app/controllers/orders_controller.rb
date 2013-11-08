3ca40ab4d21961f531188172fa55aa8bf9a98bf2
Paul Barry <mail@paulbarry.com>
2013-11-07 19:04:04 -0500

    Basic actions for checkout flow

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,7 @@
 class OrdersController < ApplicationController
-  def new
-    @order = Order.new
+  skip_before_filter :require_log_in, only: [:new, :create]
+
+  def create
+    redirect_to root_path, notice: 'Your order has been placed'
   end
 end
---
+++ test/controllers/orders_controller_test.rb
@@ -0,0 +1,16 @@
+require 'test_helper'
+
+class OrdersControllerTest < ActionController::TestCase
+  test "displays checkout form" do
+    get :new
+    assert_response :success
+    assert_select 'form'
+    assert_select 'input[type=submit]'
+  end
+
+  test "redirects to home page with notice" do
+    post :create
+    assert_redirected_to root_path
+    assert_equal "Your order has been placed", flash[:notice]
+  end
+end
