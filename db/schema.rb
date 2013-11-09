7afa83ff9e4478844f56fb46c4379fb56c6ce727
Paul Barry <mail@paulbarry.com>
2013-11-09 09:55:19 -0500

    Added credit card to checkout form

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131010204333) do
+ActiveRecord::Schema.define(version: 20131109142203) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "credit_cards", force: true do |t|
+    t.string   "cardholder_name"
+    t.string   "card_number"
+    t.integer  "exp_month"
+    t.integer  "exp_year"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "customers", force: true do |t|
     t.string   "name"
@@ -42,6 +51,7 @@ ActiveRecord::Schema.define(version: 20131010204333) do
     t.decimal  "total_amount"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "credit_card_id"
   end
 
   add_index "orders", ["customer_id"], name: "index_orders_on_customer_id", using: :btree
@@ -68,5 +78,16 @@ ActiveRecord::Schema.define(version: 20131010204333) do
     t.datetime "created_at"
     t.datetime "updated_at"
   end
+
+  create_table "versions", force: true do |t|
+    t.string   "item_type",  null: false
+    t.integer  "item_id",    null: false
+    t.string   "event",      null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+  end
+
+  add_index "versions", ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
 
 end
