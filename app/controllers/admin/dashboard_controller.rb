e23de24f190040137de8c8b5734d7d2e24a31cbc
Paul Barry <mail@paulbarry.com>
2013-12-03 20:09:54 -0500

    Use real data in the dashboard charts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -7,7 +7,9 @@ class Admin::DashboardController < ApplicationController
     where placed_at > '#{30.days.ago}'
     group by date(placed_at)
     order by date(placed_at)}
-    @days = Order.connection.select_rows(sql)
+    @days = Order.connection.select_rows(sql).map do |row|
+      [row[0], row[1].to_i, row[2].to_f]
+    end
   end
 
 end
